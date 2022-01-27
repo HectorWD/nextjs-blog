2fe40fe5276b9aa9c5487c0aac1f2447d6f24175
Hector Wong <hectorwong2110@gmail.com>
2022-01-26 20:08:19 -0700

    primer commit

---
+++ pages/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Blog = () => {
+  return(
+      <Layout>
+          Hola soy el blog
+      </Layout>
+  );
+};
+
+export default Blog;
---
+++ pages/posts/[id].js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+export default function post ({data}){
+  return (
+    <Layout>
+      <h1> {data.id} -- {data.title} </h1>
+      <p> {data.body} </p>
+      
+    </Layout>
+  )
+}
+
+// Primero:generar el path para las rutas dinamicas
+export async function getStaticPaths(){
+  try {
+    const res= await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data= await res.json();
+    
+    //Extraemos la id seleccionada
+    const paths= data.map(({id})=>(
+      {params:{id:`${id}`}}
+    ))
+    return {
+      paths,
+      fallback:false
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+//Segundo :Obtenemos la info de la id seleccioanda
+export async function getStaticProps({params}){
+    try {
+      const res= await fetch('https://jsonplaceholder.typicode.com/posts/'+params.id);
+      const data= await res.json();
+  
+      return{
+        props:{
+          data
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
